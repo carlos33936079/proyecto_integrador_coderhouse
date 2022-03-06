5d8a8b0e72f56751576090b13050bcecaa4780f1
Carlos Luna <c.l.08.cl@gmail.com>
2022-03-06 12:22:00 -0300

    itemCount, Luna

--- app/src/components/main/ItemCount.js
+++ app/src/components/main/ItemCount.js
@@ -2,25 +2,37 @@ import React from 'react'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faMinus} from '@fortawesome/free-solid-svg-icons'
 import {faPlus} from '@fortawesome/free-solid-svg-icons'
+import {useState} from "react"
 
-function ItemCount() {
+function ItemCount(props) {
 
+const [count, setCount] = useState(props.initial)
 
+const clickIncrease = () => {
+  if(count < props.stock){
+  setCount(count + 1)
+  }
+}
 
+const clickDecrement = () => {
+  if(count > 0){
+    setCount(count - 1)
+  }
+}
 
   return (
     <div className='container_card'>
         <div className='container_card_image'>
-            <img className="card_image" src='/images/images_card/no_image.jpg' alt='Image'/>
+            <img className="card_image" src='/images/images_card/no_image.jpg' alt='Imagen'/>
             <h3>Nombre Producto</h3>
         </div>
         <div className='container_count'> 
         <div className='card_count'>
-        <FontAwesomeIcon icon={faMinus} onClick={()=>alert('buton')}/>
-        <p className='numero'>0</p>
-        <FontAwesomeIcon icon={faPlus} onClick={()=>alert('buton')}/>
+        <FontAwesomeIcon icon={faMinus} onClick={clickDecrement} />
+        <p className='numero'>{count}</p>
+        <FontAwesomeIcon icon={faPlus} onClick={clickIncrease}/>
         </div>
-        <p>/50</p>
+        <p>/{props.stock}</p>
         </div>
          <br />
         <button className='button_add' onClick={()=>alert('agregar')}>Agregar al carrito</button>
--- app/src/components/main/ItemListContainer.js
+++ app/src/components/main/ItemListContainer.js
@@ -2,9 +2,14 @@ import React from 'react'
 import ItemCount from './ItemCount'
 
 function ItemListContainer({mensaje}) {
+
+const stock= 40
+const initial= 1
+const miOnAdd = () => {}
+
   return (
     <div className='item_list'>
-        <ItemCount/>
+        <ItemCount stock={stock} initial={initial} onAdd={miOnAdd}/>
     </div>
   )
 }
